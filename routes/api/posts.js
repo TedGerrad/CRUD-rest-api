7a46e206a321fbeaa49417f9ec5b9e703034af5c
TedGerrad <tedariaga@gmail.com>
2022-01-12 15:12:20 +0000

    This commit provides a modified version of posts.js and provides an accurate description of api routes and complete with routes for CRUD requests.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,27 +1,71 @@
-//Here we create our routest
+//Here we create our routes for the requests
 
 //Import express from express
 const express = require('express');
 const router = express.Router();
 //posts Model 
-const Posts = require('../../models/Posts');
+const Posts = require('../../models/Posts'); 
+
+//@routes GET api/posts
+//@desc GET ALL posts 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find();
+        if(!posts) throw Error('No items');
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(400).json({ msg: err })
+    }
+});
 
 //@routes POST api/posts
 //@desc Create a post 
 router.post('/', async (req, res) => {
-    //res.send('Lets create a new post');
-    //console.log(req.body);
+   
     const newPost = new Posts(req.body);
 
     try {
         const post = await newPost.save();
         if(!post) throw Error('Something went wrong while saving the post');
-
         res.status(200).json(post);
-
     } catch(err)  {
         res.status(400).json({ msg: err })
+    }
+}); 
 
+//@routes DELETE api/posts/:id
+//@desc DELETE A post 
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findByIdAndDelete(req.params.id);
+        if(!post) throw Error('Post not found!');
+        res.status(200).json({ success: true });
+    } catch (err) {
+        res.status(400).json({ msg: err })
+    }
+});
+
+//@routes UPDATE api/posts/:id
+//@desc update/patch A post 
+router.patch('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findByIdAndUpdate(req.params.id, req.body);
+        if(!post) throw Error('Something went wrong while updating the post!');
+        res.status(200).json({ success: true });
+    } catch (err) {
+        res.status(400).json({ msg: err })
+    }
+})
+
+//@routes GET api/posts/:id
+//@desc GET a specific post by using the id as a request parameter 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id);
+        if(!post) throw Error('No items');
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(400).json({ msg: err })
     }
 });
 
